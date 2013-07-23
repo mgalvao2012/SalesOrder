dbc860df6921e199c46b9708c090201c50052e31
Marcelo Galvao <mgalvao2012@gmail.com>
2013-07-23 00:30:59 -0300

    Update the comments

--- SalesOrder/src/com/salesorder/SalesOrder.java
+++ SalesOrder/src/com/salesorder/SalesOrder.java
@@ -5,6 +5,7 @@ import com.controller.*;
 
 public class SalesOrder {
 	
+	// Create new Sales Order into the Database
     public String pushOrder(String W_ID, String Description, String Approver){      
     	System.out.println("W_ID: " + W_ID);
     	System.out.println("Description: " + Description);
@@ -13,12 +14,14 @@ public class SalesOrder {
         return SOPersistence.pushOrder(W_ID, Description, Approver);
     }
 
+    // Check the status and justification of approval or rejection action
     public String checkOrder(String W_ID){
     	System.out.println("W_ID: " + W_ID);   	
     	SalesOrderPersistence SOPersistence = new SalesOrderPersistence();
     	return SOPersistence.checkOrder(W_ID);
     }
 
+    // Update the status and the justification of the Sales Order
     public String updateOrder(String W_ID, String Status, String Justify){      
     	System.out.println("W_ID: " + W_ID);
     	System.out.println("Status: " + Status);
@@ -27,7 +30,7 @@ public class SalesOrder {
         return SOPersistence.updateOrder(W_ID, Status, Justify);
     }
 
-    
+    // List all Sales Order for approval filtered by approver
     public String listOrder(String Approver){      
     	System.out.println("Approver: " + Approver);
     	SalesOrderPersistence SOPersistence = new SalesOrderPersistence();    	   	  	
